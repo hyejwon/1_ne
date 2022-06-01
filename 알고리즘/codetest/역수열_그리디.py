ace13c012f834167e20a52f6db8eebc9bbbf586d
“hyejwon” <hyejwon94@gmail.com>
2022-06-01 23:21:54 +0900

    0601

--- 알고리즘/codetest/역수열_그리디.py
+++ 알고리즘/codetest/역수열_그리디.py
@@ -4,7 +4,7 @@ a = list(map(int,input().split()))
 seq=[0]*(n)
 for i in range(n):
     for j in range(n):
-        if a[i]==0 and seq[j]==0:
+        if a[i]==0 and seq[j]==0:#빈공간 확보  
             seq[j]=i+1
             break # j를 break
         elif seq[j]==0:
